6bb27c9962e85b02b37d239c4cb6d6d66e359de2
Ryan Adams <radams@student.neumont.edu>
2014-05-18 20:24:01 -0600

    added components
    components classes added

---
+++ Game/Project2/Project2/Component/Powerable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Project2.Entity;
+
+namespace Project2.Component
+{
+    class Powerable: Component
+    {
+        float stealthed = 0;
+        public float shielded = 0;
+        public int score { get; set; }
+        public Powerable(Game game, Entity.Entity parent)
+            :base(game, parent)
+        {
+            score = 0;
+        }
+
+        public void stealth()
+        {
+            getComponent<Drawable3D>().turnOnStealth();
+            stealthed = 10.0f;
+        }
+
+        public void shield()
+        {
+            new Shield(Game, parent);
+        }
+
+        public void ammo()
+        {
+            getComponent<WeaponController>().bulletAmmo += WeaponController.MAX_BULLETS;
+            getComponent<WeaponController>().missileAmmo += WeaponController.MAX_MISSILES;
+        }
+
+        public void ring()
+        {
+            score++;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (stealthed > 0)
+            {
+                stealthed -= gameTime.ElapsedGameTime.Milliseconds / 1000.0f;
+                if (stealthed <= 0)
+                {
+                    getComponent<Drawable3D>().turnOffStealth();
+                }
+            }
+            base.Update(gameTime);
+        }
+    }
+}
